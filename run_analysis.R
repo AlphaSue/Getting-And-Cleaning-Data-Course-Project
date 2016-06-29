35379ab6352c5d2dd10cd0d05f0d8be6e4f731db
AlphaSue <409083575@qq.com>
2016-06-29 09:59:41 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,18 @@
-#run_analysis.R
+##run_analysis.R
 
 ##This script is trying to get and clean data.
 
 ##step 1.Merges the training and the test sets to create one data set.
-
+## read the data about test
 test_set_subject_ids_as_table<- read.table("test/subject_test.txt")
 test_set_activities_as_table<- read.table("test/y_test.txt")
 test_set_measurements<- read.table ("test/X_test.txt", sep="")
 test_table<- cbind (test_set_activities_as_table , test_set_measurements)
 colnames(test_table)[1] <-"activity"
+##change the colnames
 test_table <- cbind (test_set_subject_ids_as_table , test_table)
 colnames(test_table)[1] <- "subjectid"
+##read the data about train in the same way as reading test
 train_set_subject_ids_as_table <- read.table("train/subject_train.txt")
 train_set_activities_as_table <- read.table("train/y_train.txt")
 train_set_measurements <- read.table ("train/X_train.txt", sep="")
@@ -19,11 +21,13 @@ colnames(train_table)[1] <- "activity"
 train_table[1] <- factor(train_table[[1]])
 train_table <- cbind (train_set_subject_ids_as_table , train_table)
 colnames(train_table)[1] <- "subjectid"
+##merge data
 merged_data <- rbind (train_table, test_table)
 merged_data <- merged_data[order(merged_data$subjectid, merged_data$activity), ]
 row.names(merged_data) <- NULL
 merged_data[1] <- factor(merged_data[[1]]) 
 merged_data[2] <- factor(merged_data[[2]])
+##find out whether  there are any na values
 missing_values <- sum(is.na(merged_data))
 print(paste("There are ", missing_values," missing values in the merged data.", collapse=""))
 
